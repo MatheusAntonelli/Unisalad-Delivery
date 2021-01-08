0aea69330fb759d03e2697938ae0e07aecec78b8
matheusantonelli12@gmail.com <matheusantonelli12@gmail.com>
2021-01-08 21:45:32 -0200

    Project Created

---
+++ backend/src/main/java/com/unisalad/Unisalad/UnisaladApplication.java
@@ -0,0 +1,13 @@
+package com.unisalad.Unisalad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisaladApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnisaladApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/unisalad/Unisalad/UnisaladApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisalad.Unisalad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnisaladApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
