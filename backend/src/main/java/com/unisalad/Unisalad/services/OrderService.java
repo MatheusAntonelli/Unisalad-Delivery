20a5abf34b07fcc36fecebe1f02cb8cbfd524f37
matheusantonelli12@gmail.com <matheusantonelli12@gmail.com>
2021-01-10 02:37:56 -0300

    GET /orders

---
+++ backend/src/main/java/com/unisalad/Unisalad/services/OrderService.java
@@ -0,0 +1,29 @@
+package com.unisalad.Unisalad.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.unisalad.Unisalad.dto.OrderDTO;
+import com.unisalad.Unisalad.entities.Order;
+import com.unisalad.Unisalad.repositories.OrderRepository;
+
+
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+
+}
