bb09d3c79776de5dc2b094ddd9e57fe38d570003
matheusantonelli12@gmail.com <matheusantonelli12@gmail.com>
2021-01-16 14:20:59 -0300

    Add routes and StepsHeader

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 
 
 function Home() {
@@ -15,9 +16,8 @@ function Home() {
                         <h3 className="home-subtitle">
                             Faça o seu pedido e em poucos minutos <br /> levamos na sua porta
                   </h3>
-                        <a href="orders" className="home-btn-order">
-                            FAZER PEDIDO
-                  </a>
+                       
+                  <Link to="/orders" className ="home-btn-order">FAZER PEDIDO</Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 import { ReactComponent as Logo} from './unilogo.svg';
 
 function Navbar(){
     return( 
         <nav className = "main-navbar">
-            <a href="home" className='logo-text'><Logo/></a>
+            <Link to="/" className='logo-text'><Logo/></Link>
         </nav>
     )
 }
